a70baceb4071a7ba58bbfd7c89dc17b5a07ea9e0
İsmail Hasır <ismailhasir@gmail.com>
2022-04-26 21:54:24 +0300

    26.04

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -26,6 +26,13 @@ const firestoreReducer = (state, action) => {
         error: null,
         document: action.payload,
       };
+    case "DELETED_DOCUMENT":
+      return {
+        success: true,
+        isPending: false,
+        error: null,
+        document: null,
+      };
     default:
       return state;
   }
@@ -62,7 +69,18 @@ export const useFirestore = (collection) => {
   };
 
   // delete a document
-  const deleteDocument = async (doc) => {};
+  const deleteDocument = async (id) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      await ref.doc(id).delete();
+      dispatchIfNotCancelled({
+        type: "DELETED_DOCUMENT",
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: "could not delete" });
+    }
+  };
 
   useEffect(() => {
     return () => setIsCancelled(true);
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,25 @@
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+
+//styles
 import styles from "./Home.module.css";
+//components
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection(
+    "transactions",
+    ["uid", "==", user.uid],
+    ["createdAt", "desc"]
+  );
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Transaction List:</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
